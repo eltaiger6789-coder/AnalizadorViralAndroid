4c13db9a29fa2a639738b70385d97ccb36160c16
eltaiger6789-coder <eltaiger6789@gmail.com>
2026-07-08 23:56:00 +0200

    Update MainActivity.kt

--- app/src/main/java/com/analizadorviral/app/MainActivity.kt
+++ app/src/main/java/com/analizadorviral/app/MainActivity.kt
@@ -1,102 +1,31 @@
 package com.eltaiger.analizadorviral
 
 import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.webkit.CookieManager
-import android.webkit.ValueCallback
-import android.webkit.WebChromeClient
-import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
 
     private lateinit var webView: WebView
-    private var fileCallback: ValueCallback<Array<Uri>>? = null
-    private val FILE_CHOOSER_REQUEST = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         webView = WebView(this)
 
-        CookieManager.getInstance().setAcceptCookie(true)
-        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true)
-
         webView.settings.apply {
             javaScriptEnabled = true
             domStorageEnabled = true
-            databaseEnabled = true
-
             allowFileAccess = true
             allowContentAccess = true
-
-            cacheMode = WebSettings.LOAD_DEFAULT
-            mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
-
-            mediaPlaybackRequiresUserGesture = false
-
-            javaScriptCanOpenWindowsAutomatically = true
-            setSupportMultipleWindows(false)
-
-            userAgentString =
-                "Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 " +
-                "(KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
         }
 
         webView.webViewClient = WebViewClient()
 
-        webView.webChromeClient = object : WebChromeClient() {
-
-            override fun onShowFileChooser(
-                webView: WebView?,
-                filePathCallback: ValueCallback<Array<Uri>>?,
-                fileChooserParams: FileChooserParams?
-            ): Boolean {
-
-                fileCallback?.onReceiveValue(null)
-                fileCallback = filePathCallback
-
-                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                    addCategory(Intent.CATEGORY_OPENABLE)
-                    type = "video/*"
-                }
-
-                startActivityForResult(intent, FILE_CHOOSER_REQUEST)
-
-                return true
-            }
-        }
-
-        webView.loadUrl(
-            "https://huggingface.co/spaces/Eltaiger/analizador-viral"
-        )
-
         setContentView(webView)
-    }
 
-    @Deprecated("Deprecated in Java")
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == FILE_CHOOSER_REQUEST) {
-
-            val result =
-                if (resultCode == RESULT_OK && data?.data != null) {
-                    arrayOf(data.data!!)
-                } else {
-                    null
-                }
-
-            fileCallback?.onReceiveValue(result)
-            fileCallback = null
-        }
+        webView.loadUrl("https://eltaiger-noya-social-manager.hf.space")
     }
 
     override fun onBackPressed() {
@@ -106,9 +35,5 @@ class MainActivity : Activity() {
             super.onBackPressed()
         }
     }
-
-    override fun onDestroy() {
-        webView.destroy()
-        super.onDestroy()
-    }
 }
+
