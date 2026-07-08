cb9b080c6bc0f7d1adb317c27e148f270d537098
eltaiger6789-coder <eltaiger6789@gmail.com>
2026-07-08 02:31:43 +0200

    Update MainActivity.kt

--- app/src/main/java/com/analizadorviral/app/MainActivity.kt
+++ app/src/main/java/com/analizadorviral/app/MainActivity.kt
@@ -1,24 +1,75 @@
 package com.eltaiger.analizadorviral
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
 class MainActivity : Activity() {
 
+    private lateinit var webView: WebView
+    private var fileCallback: ValueCallback<Array<Uri>>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val webView = WebView(this)
+        webView = WebView(this)
 
         webView.settings.javaScriptEnabled = true
         webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
 
         webView.webViewClient = WebViewClient()
+
+        webView.webChromeClient = object : WebChromeClient() {
+
+            override fun onShowFileChooser(
+                webView: WebView?,
+                filePathCallback: ValueCallback<Array<Uri>>?,
+                fileChooserParams: FileChooserParams?
+            ): Boolean {
+
+                fileCallback?.onReceiveValue(null)
+                fileCallback = filePathCallback
+
+                val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+                    addCategory(Intent.CATEGORY_OPENABLE)
+                    type = "video/*"
+                }
+
+                startActivityForResult(intent, 100)
+
+                return true
+            }
+        }
 
         webView.loadUrl("https://huggingface.co/spaces/Eltaiger/analizador-viral")
 
         setContentView(webView)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            val result = if (resultCode == RESULT_OK && data?.data != null) {
+                arrayOf(data.data!!)
+            } else {
+                null
+            }
+
+            fileCallback?.onReceiveValue(result)
+            fileCallback = null
+        }
     }
 }
