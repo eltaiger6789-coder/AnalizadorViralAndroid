c70a6763fb0529f235cb2b157e598b43f995b767
eltaiger6789-coder <eltaiger6789@gmail.com>
2026-07-09 00:09:12 +0200

    Update MainActivity.kt

--- app/src/main/java/com/analizadorviral/app/MainActivity.kt
+++ app/src/main/java/com/analizadorviral/app/MainActivity.kt
@@ -2,6 +2,9 @@ package com.eltaiger.analizadorviral
 
 import android.app.Activity
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 
@@ -14,18 +17,28 @@ class MainActivity : Activity() {
 
         webView = WebView(this)
 
+        CookieManager.getInstance().setAcceptCookie(true)
+        CookieManager.getInstance()
+            .setAcceptThirdPartyCookies(webView, true)
+
         webView.settings.apply {
             javaScriptEnabled = true
             domStorageEnabled = true
+            databaseEnabled = true
             allowFileAccess = true
             allowContentAccess = true
+            cacheMode = WebSettings.LOAD_DEFAULT
+            mixedContentMode = WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE
         }
 
         webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
 
         setContentView(webView)
 
-        webView.loadUrl("https://eltaiger-noya-social-manager.hf.space")
+        webView.loadUrl(
+            "https://huggingface.co/spaces/Eltaiger/noya-social-manager"
+        )
     }
 
     override fun onBackPressed() {
@@ -36,4 +49,3 @@ class MainActivity : Activity() {
         }
     }
 }
-
